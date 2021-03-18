a049f1629c123aff3b42d6ab7924cd5bf42ee813
MikiWojak <mikizarowa8@gmail.com>
2021-03-18 15:59:39 +0100

    Edit task
    - Implement editing task by double click on existing one
    - Cancel editing when empty title left
    - Simplify cvhecking if the field is empty

--- frontend/src/components/TodoList.vue
+++ frontend/src/components/TodoList.vue
@@ -2,8 +2,11 @@
   <div>
     <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
     <div v-for="(todo, index) in todos"  :key="todo.id" class="todo-item">
-      <div>
-        {{ todo.title }}
+      <div class="todo-item-left">
+        <div v-if="!todo.editing" class="todo-item-label" @dblclick="editTodo(todo)">
+          {{ todo.title }}
+        </div>
+        <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
       </div>
 
       <div class="remove-item" @click="removeTodo(index)">
@@ -21,43 +24,74 @@ export default {
     return {
       newTodo: '',
       idForTodo: 3,
+      beforeEditCache: '',
 
       todos: [
         {
           'id': 1,
           'title': 'Finish Vue Screencast',
           'done': false,
+          'editing': false
         },
         {
           'id': 2,
           'title': 'Take over world',
           'done': false,
+          'editing': false
         }
       ]
+    }
+  },
+
+  directives: {
+    focus: {
+      // directive definition
+      inserted: function (el) {
+        el.focus()
+      }
     }
   },
 
   methods: {
     addTodo() {
       //Title is empty
-      if(this.newTodo.trim().length == 0) {
+      if(this.newTodo.trim() == '') {
         return;
       }
 
       this.todos.push({
         id: this.idForTodo,
         title: this.newTodo,
-        done: false
+        done: false,
+        editing: false
       });
 
       this.newTodo = '';
       this.idForTodo++;
     },
 
+    editTodo(todo) {
+      this.beforeEditCache = todo.title;
+      todo.editing = true;
+    },
+
+    doneEdit(todo) {
+      if(todo.title.trim() == '') {
+        todo.title = this.beforeEditCache;
+      }
+
+      todo.editing = false;
+    },
+
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
+    },
+
     removeTodo(index) {
       this.todos.splice(index, 1);
     }
-  }
+  },
 }
 </script>
 
@@ -86,7 +120,27 @@ export default {
 
   }
 
-  .remove-item:hover {
+  &:hover {
     color: black;
+  }
+
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+
+  &:focus {
+      outline: none;
   }
 </style>
