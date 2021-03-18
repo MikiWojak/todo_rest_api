54c8af1342e072b58c7b73bd0eee8dd157a13a0b
MikiWojak <mikizarowa8@gmail.com>
2021-03-18 10:46:24 +0100

    Add and delete Task to do
    - Install SASS
    - Add list of tasks
    - Implement adding tasks
    - Implement deleting one task

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -36,6 +36,7 @@ body {
   /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24px;
 }
 
 .logo {
--- frontend/src/components/TodoList.vue
+++ frontend/src/components/TodoList.vue
@@ -1,14 +1,92 @@
 <template>
   <div>
-    Todo list goes here
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos"  :key="todo.id" class="todo-item">
+      <div>
+        {{ todo.title }}
+      </div>
+
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'todo-list',
-  props: {
-    msg: String
+
+  data() {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish Vue Screencast',
+          'done': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over world',
+          'done': false,
+        }
+      ]
+    }
+  },
+
+  methods: {
+    addTodo() {
+      //Title is empty
+      if(this.newTodo.trim().length == 0) {
+        return;
+      }
+
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        done: false
+      });
+
+      this.newTodo = '';
+      this.idForTodo++;
+    },
+
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
 }
 </script>
+
+<style>
+  .todo-input {
+    width: 98%;
+    padding: 1% 1%;
+    font-size: 18px;
+    margin-bottom: 16px;
+  }
+
+  &:focus {
+    outline: 0;
+  }
+
+  .todo-item {
+    display: flex;
+    margin-bottom: 12px;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+
+  }
+
+  .remove-item:hover {
+    color: black;
+  }
+</style>
