b2452f4c2bb0a60b46f4d92ba66fe9efc9723ae9
MikiWojak <mikizarowa8@gmail.com>
2021-03-18 18:56:02 +0100

    TodoItem component
    - Add the TodoItem component
    - Move template and data to the TodoItem
    - Methods not moved yet

---
+++ frontend/src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-item-left">
+      <input type="checkbox" v-model="done">
+      <div v-if="!editing" class="todo-item-label" :class="{ completed: done }" @dblclick="editTodo">
+        {{ title }}
+      </div>
+      <input v-else class="todo-item-edit" type="text" v-model="title" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus>
+    </div>
+
+    <div class="remove-item" @click="removeTodo(index)">
+      &times;
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      'id': this.todo.id,
+      'title': this.todo.title,
+      'done': this.todo.done,
+      'editing': this.todo.editing,
+      'beforeEditCache': ''
+    }
+  }
+}
+</script>
