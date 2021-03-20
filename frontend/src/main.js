818469784b5d4ceccc43557c2a13b9e8d0e8dfc9
MikiWojak <mikizarowa8@gmail.com>
2021-03-20 19:40:36 +0100

    Routes - prepare
    - Add new vue files
    - Prepare layout for the site
    - Delete unnecessary files
    - Install vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import Master from './components/layouts/Master'
 import { store } from './store/store'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
 new Vue({
   store: store,
-  render: h => h(App),
+  render: h => h(Master),
 }).$mount('#app')
