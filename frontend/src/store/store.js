2fb885b4354110ef3f16f80a144870e6993632f6
MikiWojak <mikizarowa8@gmail.com>
2021-03-20 15:24:57 +0100

    Retrieve Todos from DB
    - Install axios in the Frontend
    - Code retrieving Todos from the DB
    - Install CORS in the backend
    - Hide some columns of Todo

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,24 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex);
+axios.defaults.baseURL = 'http://localhost:8000/api';
 
 export const store = new Vuex.Store({
     state: {
-        todos: [
-            {
-                'id': 1,
-                'title': 'Finish Vue Screencast',
-                'done': false,
-                'editing': false
-            },
-            {
-                'id': 2,
-                'title': 'Take over world',
-                'done': false,
-                'editing': false
-            }
-        ]
+        todos: []
     },
 
     getters: {
@@ -47,10 +36,24 @@ export const store = new Vuex.Store({
         deleteTodo(state, id) {
             const index = state.todos.findIndex((item) => item.id == id);
             state.todos.splice(index, 1);
+        },
+
+        retrieveTodos(state, todos) {
+            state.todos = todos;
         }
     },
 
     actions: {
+        retrieveTodos(context) {
+            axios.get('/todos')
+                .then(response => {
+                    context.commit('retrieveTodos', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+
         addTodo(context, todo) {
             context.commit('addTodo', todo);
         },
