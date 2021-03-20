278cf80f8071b58be76c9c2d46f38461976c48d8
MikiWojak <mikizarowa8@gmail.com>
2021-03-20 20:19:01 +0100

    Routes
    - Routes to the main page and list of tasks

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import App from './App'
+import LandingPage from './components/marketing/LandingPage'
 import Master from './components/layouts/Master'
 import { store } from './store/store'
 
@@ -7,7 +9,17 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', component: LandingPage },
+  { path: '/todo', component: App }
+];
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router: router,
   store: store,
   render: h => h(Master),
 }).$mount('#app')
