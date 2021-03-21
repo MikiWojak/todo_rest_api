adc13db7af19039baaf640c245ff6a76a65773e8
MikiWojak <mikizarowa8@gmail.com>
2021-03-21 18:17:40 +0100

    Correct update
    - Correct updating status of the task

--- backend/app/Models/Todo.php
+++ backend/app/Models/Todo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    protected $fillable = ['title', 'completed'];
+    protected $fillable = ['title', 'done'];
 
     protected $hidden = ['created_at', 'updated_at'];
 }
